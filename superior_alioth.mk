cac8132a397851c75345c341342fce0f54b6c72b
ashutoshchettri <eidoronp67@gmail.com>
2024-02-24 07:54:24 +0545

    alioth: Bringup for @SuperiorExtended

--- superior_alioth.mk
+++ superior_alioth.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SupExt stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := superior_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -22,6 +22,12 @@ PRODUCT_MODEL := POCO F3
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
