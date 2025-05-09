40efbd2125495e025c94a184893c5e5fa1a97bc7
pawelik001 <pawelik001@gmail.com>
2025-05-09 15:26:09 +0200

    alioth: Update kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,4 @@ base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/conf
 git clone https://gitlab.com/dark.phnx12/vendor_xiaomi_camera -b aosp-14 vendor/xiaomi/camera --depth=1
 git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8250-common -b 15 device/xiaomi/sm8250 --depth=1
 git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8250-common -b 15 vendor/xiaomi/sm8250 --depth=1
-git clone https://github.com/Infinity-X-Devices/kernel_xiaomi_sm8250 -b 15 kernel/xiaomi/sm8250 --depth=1
+git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250-16.1.0 -b 15 kernel/xiaomi/sm8250 --depth=1
