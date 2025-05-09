ded793fbb04bdb450cbac496a3ab33a3a7aebb3a
pawelik001 <pawelik001@gmail.com>
2025-05-09 15:24:09 +0200

    alioth: Modify vendorsetup.sh for CI build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+git clone https://gitlab.com/dark.phnx12/vendor_xiaomi_camera -b aosp-14 vendor/xiaomi/camera --depth=1
+git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8250-common -b 15 device/xiaomi/sm8250 --depth=1
+git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8250-common -b 15 vendor/xiaomi/sm8250 --depth=1
+git clone https://github.com/Infinity-X-Devices/kernel_xiaomi_sm8250 -b 15 kernel/xiaomi/sm8250 --depth=1
