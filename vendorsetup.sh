471954520fb4a7ce2f185d12033e8a5059c72e99
pawelik001 <pawelik001@gmail.com>
2025-05-10 16:33:19 +0200

    alioth: Clone repositories to proper directory

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,9 +2,9 @@
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
 git clone https://gitlab.com/dark.phnx12/vendor_xiaomi_camera -b aosp-14 vendor/xiaomi/camera --depth=1
-git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8250-common -b 15 device/xiaomi/sm8250 --depth=1
+git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8250-common -b 15 device/xiaomi/sm8250-common --depth=1
 git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_alioth -b 15 vendor/xiaomi/alioth --depth=1
-git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8250-common -b 15 vendor/xiaomi/sm8250 --depth=1
+git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8250-common -b 15 vendor/xiaomi/sm8250-common --depth=1
 git clone https://github.com/Sanjis-Android-Playground/kernel_sm8250-16.1.0 -b 15 kernel/xiaomi/sm8250 --depth=1
 
 cd system/libhwbinder && git fetch https://github.com/custom-crdroid/system_libhwbinder/ && git cherry-pick d9d46e78cec0d09498fd5890eed9f7195baed0fd && croot
